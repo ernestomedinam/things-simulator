0e0e2b72be62b0b226f124f3d3efda4c0b90b965
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2020-11-24 23:27:52 -0400

    Work on simulate things spawning freature
    - random items created on iteration over input 'times' variable.
    
    - adjust board height thinking about info section on dashboard.

--- src/start.js
+++ src/start.js
@@ -1,5 +1,6 @@
 import { createThing } from "./things";
 import { Thing } from "./Thing";
+import { getRandomColor } from "./utils";
 
 const refreshSimulator = () => {
   let board = document.querySelector(".board");
@@ -19,26 +20,68 @@ const createButton = (name, classNames) => {
   button.classList.add("button", ...classNames);
   button.textContent = name;
   if (name == "spawn") {
-    button.addEventListener("click", () => {
-      handleSpawn();
+    button.addEventListener("click", e => {
+      handleSpawn(e);
+      refreshSimulator();
+    });
+  } else if (name == "sim spawn") {
+    button.addEventListener("click", e => {
+      handleSimulateSpawn(e);
       refreshSimulator();
     });
   }
   return button;
 };
 
-const handleSpawn = event => {
+const handleSimulateSpawn = event => {
+  // get ahold of the input to see if there is
+  // a valid number of times for the simulation
+  let input = document.querySelector("input");
+  let times = 0;
+  console.log(parseInt(input.value));
+  if (input.value != "" && !isNaN(parseInt(input.value))) {
+    times = parseInt(input.value);
+  } else {
+    do {
+      times = parseInt(
+        window
+          .prompt("How many random spawns would you like to simulate?")
+          .trim()
+      );
+      console.log(times, " ", !isNaN(times));
+    } while (isNaN(times) || times == 0);
+  }
+  for (let time = 0; time < times; time++) {
+    // spawn a thing with a random color and a name
+    handleSpawn(event, `random thing #${time}`, getRandomColor());
+  }
+};
+
+const handleSpawn = (event, name = "", color = "") => {
   console.log("requested to spawn a thing (?)");
   // validate if there is a thing name written in the input
   // when the button was clicked
   let input = document.querySelector("input");
   // if there is we use that name, otherwise we ask for it
-  let name =
-    input.value != "" ? input.value : window.prompt("what's the thing's name?");
+  // changing this to if statement instead of conditional expresion
+  if (name == "") {
+    if (input.value != "") {
+      name = input.value;
+    } else {
+      do {
+        name = window.prompt("what's the thing's name?").trim();
+      } while (name == "");
+    }
+  }
   // clean the input either way
   input.value = "";
   // ask for its color...
-  let color = window.prompt("what's its color?");
+  // and also changing this to add possibility of receiving a color
+  if (color == "") {
+    do {
+      color = window.prompt("what's its color?").trim();
+    } while (color == "");
+  }
   // grab the board
   let board = document.querySelector(".board");
   // let's create a thing! creating a thing means
@@ -67,6 +110,8 @@ export const createDashboard = () => {
   dashboard.children[0].appendChild(input);
   let spawn = createButton("spawn", ["action", "super"]);
   dashboard.children[1].appendChild(spawn);
+  let simulateSpawn = createButton("sim spawn", ["primary", "super"]);
+  dashboard.children[1].appendChild(simulateSpawn);
   return dashboard;
 };
 
@@ -74,7 +119,7 @@ export const createBoard = () => {
   window.variables = {
     board: {
       step: "5px",
-      height: "720px",
+      height: "608px",
       width: "960px"
     },
     things: []
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,24 @@
 export const getRandomCompliment = () => {
-    let compliments = ["gorgeous", "kind of weirdish", "awesome", "luxurious", "ridiculous", "amazing", "superb", "maybe sad (?)"];
-    return compliments[Math.floor(Math.random() * compliments.length)];
-}
+  let compliments = [
+    "gorgeous",
+    "kind of weirdish",
+    "awesome",
+    "luxurious",
+    "ridiculous",
+    "amazing",
+    "superb",
+    "maybe sad (?)"
+  ];
+  return compliments[Math.floor(Math.random() * compliments.length)];
+};
+
+export const getRandomColor = (asString = true, opacity = 1) => {
+  let r = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  if (asString) {
+    return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+  } else {
+    return { r, b, g, opacity };
+  }
+};
